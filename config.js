51d1aaabb1f8ee333473f70ae26f04a544bd0ee8
ompaarmar336 <85385415+ompaarmar336@users.noreply.github.com>
2024-10-06 16:29:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,99 @@
+window.PorfolioConfig = {
+    name: 'OM PARMAR',
+    jobTitle: 'Cybersecurity Engineer',
+    jobDescription: 'A cybersecurity enthusiast with a deep curiosity for understanding and solving the complex puzzles of digital security. Driven by a passion for innovation and a commitment to staying ahead of evolving threats, I thrive on exploring creative solutions and implementing cutting-edge security measures. My approach to cybersecurity blends analytical rigor with a proactive mindset, aiming to anticipate and mitigate risks before they impact systems. With a keen eye for detail and a dedication to continuous learning, I’m excited about contributing to the ever-evolving field of cybersecurity and making a tangible impact on safeguarding digital environments.',
+    skills: [
+      {
+        name: 'Language',
+        description: 'C/C++, Python, Bash, HTML/CSS.'
+      },
+      {
+        name: 'Tools',
+        description: 'GitHub, Unix Shell, Nessus, Splunk, Volatility, AutoSpy, Metasploit, Wireshark, Nmap, BurpSuite, Snort, Cain and Abel, Nikto, Aircrack-ng, VSCode.'
+      },
+      {
+        name: 'Technical Skill',
+        description: 'Penetration Testing, Cloud Computing, Ethical Hacking, Steganography, Cryptography, Cybersecurity Analyst.'
+      }
+    ],
+    works: [
+      {
+        name: 'Honeypot',
+        link: '',
+        description: 'I developed and deployed a honeypot system using Cowrie, configured to mimic a real network environment to attract and analyze unauthorized access attempts. I analyzed the collected data to identify attack patterns and techniques, using insights to enhance network security by identifying vulnerabilities and implementing preventive measures with Snort and Suricata. Key features of the system include logging, monitoring, reporting, threat analysis, threat detection, network security, and attack pattern identification. '
+      },
+      {
+        name: 'Log Analyzer',
+        link: 'https://github.com/jainammutha/Log-Analyser',
+        description: 'I enabled real-time monitoring and analysis of system and application logs, identifying anomalies, security incidents, and performance issues using machine learning algorithms and custom scripts. I designed and implemented a log analyzer tool using Python to aggregate, parse, and visualize log data from various sources. Key features of the tool include real-time log aggregation, anomaly detection, automated reporting, custom alerting, and enhanced incident response.'
+      },
+      {
+        name: 'Encryption and Decryption',
+        link: 'https://github.com/ompaarmar336/GIR',
+        description: 'I created encryption and decryption algorithms that encrypt data with a different set of strings each time, while ensuring the original string remains the same when decrypted. Key features of the system include randomness, string manipulation, and integration with Tkinter for the encryption and decryption processes.'
+      }
+    ],
+    hobbies: [
+      {
+        name: 'Certified Ethical Hacking (CEH)',
+        description: 'This certification is provied by EC-Council and it certification validates my ability to identify and secure vulnerabilities in systems using the same tools and techniques as malicious hackers. It equips me with the skills to think like a hacker and protect organizations from cyber threats.'
+      },
+      {
+        name: 'Certified Network Defender (CND)',
+        description: 'This certification is provied by EC-Council and itdemonstrates my expertise in protecting, detecting, and responding to network security threats. It equips me with the skills to safeguard networks and ensure the integrity, confidentiality, and availability of data.'
+      },
+      {
+        name: 'Ethical Hacking Essentials (EHE)',
+        description: 'This certification is provied by EC-Council and it provides foundational knowledge in ethical hacking principles and practices. It equips me with the essential skills to identify vulnerabilities and apply effective countermeasures to protect systems and networks.'
+      },
+      {
+        name: 'Introduction to Cybersecurity',
+        description: 'This certification is provied by EC-Council and i provided me with a solid foundation in essential cybersecurity concepts and practices. It equipped me with the fundamental knowledge needed to understand, identify, and address various cyber threats and vulnerabilities. This certification underscores my commitment to building a secure digital environment and serves as a stepping stone in my journey to mastering the complexities of cybersecurity.'
+      },
+      {
+        name: 'Digital Foransic Essentials (DFE)',
+        description: 'This certification is provied by EC-Council and it is equipped me with critical skills in digital forensics, focusing on the principles and techniques essential for investigating and analyzing digital evidence. This certification enhances my ability to handle and interpret data in a manner that supports legal and organizational requirements, laying a strong foundation for conducting thorough and accurate digital investigations.'
+      },
+      {
+        name: 'Network Defense Essentials (NDE)',
+        description: 'This certification is provied by EC-Council and it provides me with core knowledge and skills in safeguarding network infrastructures against a range of threats. This certification emphasizes key principles and practices for protecting, detecting, and responding to network security incidents, reinforcing my ability to implement effective defense strategies and enhance overall network resilience.'
+      },
+      {
+        name: 'Certified Cybersecurity (CC)',
+        description: 'This certification is provied by ISC2 and it has solidified my understanding of fundamental cybersecurity principles and practices. This certification demonstrates my ability to manage and mitigate cyber risks effectively, employing a comprehensive approach to protect information systems and ensure organizational security. It underscores my commitment to upholding industry standards and best practices in the field of cybersecurity.'
+      },
+      {
+        name: 'OWASP API Security Top 10',
+        description: 'This certification is provied by APISec University and it has deepened my understanding of the most critical security risks associated with APIs. This certification highlights my ability to identify and address common vulnerabilities and threats in API development and deployment, ensuring robust security measures are in place to protect against potential exploits and data breaches.'
+      },
+      {
+        name: 'Cybersecurity By Google',
+        description: 'This certification is provied by Google and it has equipped me with essential skills and knowledge in modern cybersecurity practices, leveraging Google expertise in the field. This certification provides a strong foundation in protecting digital assets, understanding threat landscapes, and implementing effective security measures to safeguard systems and data. It underscores my commitment to applying industry-leading strategies to ensure robust cybersecurity.'
+      },
+      {
+        name: 'OSForensic Triage Certification (OSFTC)',
+        description: 'This certification has enhanced my expertise in using OSForensic tools for efficient digital forensic analysis. This certification demonstrates my ability to conduct comprehensive triage operations, analyze digital evidence, and extract critical information effectively. It underscores my proficiency in applying forensic techniques to support investigations and ensure accurate, actionable results.'
+      },
+      {
+        name: ' Cyber Threat Intelligence by IBM ',
+        description: 'This certification has enhanced my expertise in using OSForensic tools for efficient digital forensic analysis. This certification demonstrates my ability to conduct comprehensive triage operations, analyze digital evidence, and extract critical information effectively. It underscores my proficiency in applying forensic techniques to support investigations and ensure accurate, actionable results.'
+      },
+    ],
+    contacts: {
+      email: 'omparmar336@gmail.com',
+      links: [
+        {
+          name: 'GitHub',
+          link: 'https://github.com/ompaarmar336'
+        },
+        {
+          name: 'Linkend',
+          link: 'www.linkedin.com/in/omparmar2002'
+        },
+        {
+          name: 'Try Hack Me',
+          link: 'https://tryhackme.com/p/omparmar336'
+        }
+      ]
+    }
+  };
